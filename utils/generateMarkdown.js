d3cba39a9d3722f60ea04ebc30bfb9185fef8465
Zach Yarbrough <zachyarbro@gmail.com>
2021-08-27 14:17:27 -0500

    fixed template to format README correctly

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,49 +2,49 @@ function renderLicenseBadge(license) {
     if(license === 'MIT') {
         return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
     } else if(license === 'None') {
-      return '';
+        return '';
     }
-    }
+}
 
-    function renderLicenseLink(license) {
-      if(license !== 'None') {
+function renderLicenseLink(license) {
+    if(license !== 'None') {
         return `- [License](#license)`;
-      } else {
+    } else {
         return '';
-      }
     }
+}
 
-    function renderLicenseSection(license) {
-      if(license !== 'None') {
+function renderLicenseSection(license) {
+    if(license !== 'None') {
         return `## License
-        ${renderLicenseBadge(license)}  
-        `
-      } else {
+${renderLicenseBadge(license)}  
+`
+    } else {
         return '';
-      }
     }
+}
 
-    function generateMarkdown(data) {
-      return `# ${data.title}
-    ## Description 
-    ${data.about}
-    ## Table of Contents
-      - [Installation](#installation)
-      - [Usage](#usage)
-      ${renderLicenseLink(data.license)}
-      - [Contributing](#contributing)
-      - [Tests](#tests)
-    ## Installation
-    ${data.install}
-    ## Usage
-    ${data.usage}
-    ${renderLicenseSection(data.license)}
-    ## Contributing
-    ${data.contribution}
-    ## Tests
-    ${data.test}
-    `;
-    }
-    
+function generateMarkdown(data) {
+    return `# ${data.title}
+## Description 
+${data.about}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+${renderLicenseLink(data.license)}
+- [Contributing](#contributing)
+- [Tests](#tests)
+## Installation
+${data.install}
+## Usage
+${data.usage}
+${renderLicenseSection(data.license)}
+## Contributing
+${data.contribution}
+## Tests
+${data.test}
+`;
+}
+
     module.exports = generateMarkdown;
     
